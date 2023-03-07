2bfb058ba22ee7c3d23acae80f26902cf3c7968f
Mitsuharu Emoto <mthr1982@gmail.com>
2023-03-07 13:01:26 +0900

    update rn-screens

--- android/app/src/main/java/com/mobpri/MainActivity.java
+++ android/app/src/main/java/com/mobpri/MainActivity.java
@@ -5,6 +5,9 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+// react-native-screens
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -14,6 +17,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "MobPri";
+  }
+
+  // react-native-screens
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
   }
 
   /**
