c53b54302f5d25827c3665e2be828cb43acda0a7
Mitsuharu Emoto <mthr1982@gmail.com>
2023-11-24 22:31:55 +0900

    add patch

--- src/utils/CONSTANTS.ts
+++ src/utils/CONSTANTS.ts
@@ -2,8 +2,18 @@
  * アプリで取り扱う BASE64 のパラメータ
  */
 export const BASE64 = {
-  // 最大は 384
+  /**
+   * 名刺版のアイコンサイズ
+   */
   SIZE: 200,
 
+  /**
+   * 印刷可能な最大横幅
+   */
+  MAX_SIZE: 384,
+
+  /**
+   * BASE64用のプレフィックス
+   */
   PREFIX: 'data:image/png;base64,',
 }
