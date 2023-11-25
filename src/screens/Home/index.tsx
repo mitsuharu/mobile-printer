621babbcf7ac35e74a8e7c7517ffbb8e3f9fb28e
Mitsuharu Emoto <mthr1982@gmail.com>
2023-11-25 15:52:59 +0900

    fix home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import {
   printImageFromImagePicker,
   printProfile,
+  printText,
 } from '@/redux/modules/printer/slice'
 import { Cell, Section } from '@/components/List'
 import { useNavigation } from '@react-navigation/native'
@@ -13,21 +14,30 @@ import { EditToggleButton } from '@/components/Button/EditToggleButton'
 import { sampleProfile } from '@/redux/modules/printer/utils/sample'
 import { selectPrinterSubmissions } from '@/redux/modules/printer/selectors'
 import { createSubmission, Submission } from '@/redux/modules/printer/utils'
+import { InputDialog } from '@/components/Dialog'
 
 type Props = {}
 type ComponentProps = Props & {
+  isVisibleDialog: boolean
   isEditable: boolean
   submissions: Submission[]
+  onPressDialog: (text: string) => void
+  onCancelDialog: () => void
   onPressSample: () => void
+  onPressTextFromAlertInput: () => void
   onPressImageFromImagePicker: () => void
   onPressSubmission: (obj: Submission) => void
   onPressNewSubmission: () => void
 }
 
 const Component: React.FC<ComponentProps> = ({
+  isVisibleDialog,
   isEditable,
   submissions,
+  onPressDialog,
+  onCancelDialog,
   onPressSample,
+  onPressTextFromAlertInput,
   onPressImageFromImagePicker,
   onPressSubmission,
   onPressNewSubmission,
@@ -35,41 +45,57 @@ const Component: React.FC<ComponentProps> = ({
   const styles = useStyles()
 
   return (
-    <ScrollView style={styles.scrollView}>
-      <Section title="汎用印刷">
-        <Cell title="画像を印刷する" onPress={onPressImageFromImagePicker} />
-      </Section>
-      <Section title="プロフィールを印刷する">
-        {submissions.map((submission) => (
+    <>
+      <ScrollView style={styles.scrollView}>
+        <Section title="汎用印刷">
           <Cell
-            title={submission.title}
-            onPress={() => onPressSubmission(submission)}
-            accessory={isEditable ? 'disclosure' : undefined}
-            key={submission.uuid}
+            title="テキストを印刷する"
+            onPress={onPressTextFromAlertInput}
           />
-        ))}
-      </Section>
-      <Section>
-        <Cell title="サンプルプロフィールを印刷する" onPress={onPressSample} />
-        {isEditable ? (
+          <Cell title="画像を印刷する" onPress={onPressImageFromImagePicker} />
+        </Section>
+        <Section title="プロフィールを印刷する">
+          {submissions.map((submission) => (
+            <Cell
+              title={submission.title}
+              onPress={() => onPressSubmission(submission)}
+              accessory={isEditable ? 'disclosure' : undefined}
+              key={submission.uuid}
+            />
+          ))}
+        </Section>
+        <Section>
           <Cell
-            title="プロフィールを追加する"
-            onPress={onPressNewSubmission}
-            accessory={'disclosure'}
+            title="サンプルのプロフィールを印刷する"
+            onPress={onPressSample}
           />
-        ) : null}
-      </Section>
-    </ScrollView>
+          {isEditable ? (
+            <Cell
+              title="プロフィールを追加する"
+              onPress={onPressNewSubmission}
+              accessory={'disclosure'}
+            />
+          ) : null}
+        </Section>
+      </ScrollView>
+      <InputDialog
+        isVisible={isVisibleDialog}
+        title="テキスト印刷"
+        description="印刷するテキストを入力してください"
+        onPress={onPressDialog}
+        onCancel={onCancelDialog}
+      />
+    </>
   )
 }
 
 const Container: React.FC<Props> = (props) => {
   const navigation = useNavigation()
   const dispatch = useDispatch()
-
   const submissions: Submission[] = useSelector(selectPrinterSubmissions)
 
   const [isEditable, setIsEditable] = useState<boolean>(false)
+  const [isVisibleDialog, setIsVisibleDialog] = useState<boolean>(false)
 
   const toggle = useCallback(() => {
     setIsEditable(!isEditable)
@@ -77,7 +103,7 @@ const Container: React.FC<Props> = (props) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: 'モバイル名刺印刷',
+      title: 'モバイル印刷',
       // eslint-disable-next-line react/no-unstable-nested-components
       headerRight: () => (
         <EditToggleButton isEditable={isEditable} toggle={toggle} />
@@ -88,6 +114,22 @@ const Container: React.FC<Props> = (props) => {
   const onPressSample = useCallback(() => {
     dispatch(printProfile(sampleProfile))
   }, [dispatch])
+
+  const onPressTextFromAlertInput = useCallback(() => {
+    setIsVisibleDialog(true)
+  }, [setIsVisibleDialog])
+
+  const onPressDialog = useCallback(
+    (text: string) => {
+      setIsVisibleDialog(false)
+      dispatch(printText({ text: text, size: 'default' }))
+    },
+    [setIsVisibleDialog, dispatch],
+  )
+
+  const onCancelDialog = useCallback(() => {
+    setIsVisibleDialog(false)
+  }, [setIsVisibleDialog])
 
   const onPressImageFromImagePicker = useCallback(() => {
     dispatch(printImageFromImagePicker())
@@ -112,9 +154,13 @@ const Container: React.FC<Props> = (props) => {
     <Component
       {...props}
       {...{
+        isVisibleDialog,
         isEditable,
         submissions,
+        onPressDialog,
+        onCancelDialog,
         onPressSample,
+        onPressTextFromAlertInput,
         onPressImageFromImagePicker,
         onPressSubmission,
         onPressNewSubmission,
