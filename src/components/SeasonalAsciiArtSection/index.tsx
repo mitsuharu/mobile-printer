d02ee033bb7da0323f8eb77f27615c7dd90415f4
Mitsuharu Emoto <mthr1982@gmail.com>
2023-12-15 22:26:45 +0900

    fix import

--- src/components/SeasonalAsciiArtSection/index.tsx
+++ src/components/SeasonalAsciiArtSection/index.tsx
@@ -4,7 +4,6 @@ import { useSelector, useDispatch } from 'react-redux'
 import { selectAsciiArtSeasonalEvent } from '@/redux/modules/asciiArt/selectors'
 import {
   SeasonalEvent,
-  printAsciiArt,
   printAsciiArtChristmas,
   printAsciiArtNewYear,
   printAsciiArtSnow,
